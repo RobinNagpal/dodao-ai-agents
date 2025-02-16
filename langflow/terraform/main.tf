a65d88b08e9e416d3b0162c6d91f3b4d43f23109
robin <robinnagpal.tiet@gmail.com>
2025-02-15 21:55:04 -0500

    Move and reorganize Terraform files
    Reorganized Terraform files by consolidating and renaming directories to improve structure and clarity. Removed obsolete `new-terraform` folder and updated `.gitignore` to reflect changes.

--- langflow/terraform/main.tf
+++ langflow/terraform/main.tf
@@ -1,13 +1,9 @@
-###############################################################################
-# terraform/main.tf
-###############################################################################
-
 terraform {
   required_version = ">= 1.0.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.54.1"
+      version = "~> 5.54.1"
     }
   }
 }
@@ -15,5 +11,3 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
-
-# Optionally, you can define your backend or remote state config here.
