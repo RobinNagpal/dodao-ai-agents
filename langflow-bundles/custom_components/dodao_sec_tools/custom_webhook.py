c5a9edd63f5afe5a9b172128c4f061231e14ba31
DawoodMehmood <dawoodmehmood52222@gmail.com>
2025-04-11 23:10:36 +0500

    update custom webhook component name

--- langflow-bundles/custom_components/dodao_sec_tools/custom_webhook.py
+++ langflow-bundles/custom_components/dodao_sec_tools/custom_webhook.py
@@ -7,9 +7,9 @@ from langflow.schema.message import Message
 
 
 class WebhookComponent(Component):
-    display_name = "Webhook"
+    display_name = "Custom Webhook"
     description = "Defines a webhook input for the flow."
-    name = "Webhook"
+    name = "Custom Webhook"
     icon = "webhook"
 
     inputs = [
